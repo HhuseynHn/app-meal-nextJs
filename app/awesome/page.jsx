2170e5a8ac84885f5ed57044c51b0edb5c5ef337
Dell Latitude E5570 <hhuseyn573@gmail.com>
2025-03-25 17:56:42 +0400

    first commit

---
+++ app/awesome/page.jsx
@@ -0,0 +1,13 @@
+/** @format */
+import React from "react";
+
+const TestPage = () => {
+  return (
+    <>
+      <main>
+        <h1>Next js awesome</h1>
+      </main>
+    </>
+  );
+};
+export default TestPage;
